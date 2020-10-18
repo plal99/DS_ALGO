d78ee8bd6151c9c2121586eb5481639e165c5882
plal <lal.praveen123@gmail.com>
2020-10-18 20:43:54 +0530

    Template

---
+++ towerOfHanoi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void hanoi(int n, char source, char destination, char auxilary);
+
+int main()
+{
+    int n = 4;
+    hanoi(n, 'A', 'B', 'C');
+    return 0;
+}
+
+void hanoi(int n, char source, char destination, char auxilary)
+{
+    
+}
