2be43d7c7775ff3f914fad59d50e3eee087974c9
plal <lal.praveen123@gmail.com>
2020-10-18 20:58:03 +0530

    Completed code

--- towerOfHanoi.cpp
+++ towerOfHanoi.cpp
@@ -5,12 +5,21 @@ void hanoi(int n, char source, char destination, char auxilary);
 
 int main()
 {
-    int n = 4;
+    int n;
+    cout << "Enter number of disks: ";
+    cin >> n;
     hanoi(n, 'A', 'B', 'C');
     return 0;
 }
 
 void hanoi(int n, char source, char destination, char auxilary)
 {
-    
+    if (n == 1)
+    {
+        cout << "Move 1 from " << source << " to " << destination << "\n";
+        return;
+    }
+    hanoi(n - 1, source, auxilary, destination);
+    cout << "Move " << n << " from " << source << " to " << destination << "\n";
+    hanoi(n - 1, auxilary, destination, source);
 }
