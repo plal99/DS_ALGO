0ef12777b1ccbf4efbf21a7dfb4938042f63a928
plal <lal.praveen123@gmail.com>
2020-10-28 19:08:03 +0530

    Code almost completed

---
+++ jobSeqWithDeadline.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Job
+{
+    char id;
+    int deadline;
+    int profit;
+};
+
+bool comparison(Job a, Job b);
+void jobSequencing(Job arr[], int num);
+
+int main()
+{
+    int num;
+    cin >> num;
+    Job arr[num];
+    for (int i = 0; i < num; i++)
+    {
+        cin >> arr[i].id;
+        cin >> arr[i].profit;
+        cin >> arr[i].deadline;
+    }
+
+    jobSequencing(arr, num);
+}
+
+
+bool comparison(Job a, Job b)
+{
+    return (a.profit > b.profit);
+}
+
+
+void jobSequencing(Job arr[], int num)
+{
+    sort(arr, arr + num, comparison);
+
+    int result[num];
+    
+    bool slot[num];
+    memset(slot, 0, sizeof(slot));
+
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = min(num, arr[i].deadline); j >= 0; j--)
+        {
+            if(slot[j] == false)
+            {
+                result[j] = i;
+                slot[j] = true;
+                break;
+            }
+        }
+    }
+
+    for (int i=0; i<num; i++)
+    {
+       if (slot[i] == true)
+        cout << arr[result[i]].id << " ";
+    }
+}
