9e4f893717e577d50161cec88a2cfcb4de651ec9
plal <lal.praveen123@gmail.com>
2020-10-30 00:08:47 +0530

    Matrix chain multiplication

---
+++ matrixChain.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[4][4];
+
+int matrixChain(int arr[], int n, int i, int j);
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    memset(dp, -1, sizeof(dp));
+    int temp = matrixChain(arr, n, 1, n - 1);
+    cout << temp;
+}
+
+int matrixChain(int arr[], int n, int i, int j)
+{
+    if (i == j)
+        return 0;
+    if (dp[i][j] != -1)
+        return dp[i][j];
+
+    dp[i][j] = INT_MAX;
+    for (int k = i; k < j; k++)
+        dp[i][j] = min(dp[i][j], matrixChain(arr, n, i, k) + matrixChain(arr, n, k + i, j) + arr[i - 1] * arr[k] * arr[j]);
+    // c[i-j] = min(c[i-k] + c[k+1 - j] + d(i-1)*d(k)*d(j))
+
+    return dp[i][j];
+}
