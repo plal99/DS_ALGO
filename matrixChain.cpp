2d67d16544ea4d6b85fb6676e91150252b3df55a
plal <lal.praveen123@gmail.com>
2020-10-30 23:08:37 +0530

    Matrix chain multiplication - Most optimal

--- matrixChain.cpp
+++ matrixChain.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dp[4][4];
+int dp[100][100];
 
 int matrixChain(int arr[], int n, int i, int j);
 
 int main()
 {
-    int arr[] = {1, 2, 3, 4};
+    int arr[] = {3,2,4,2,5};
     int n = sizeof(arr) / sizeof(arr[0]);
     memset(dp, -1, sizeof(dp));
     int temp = matrixChain(arr, n, 1, n - 1);
@@ -21,7 +21,7 @@ int matrixChain(int arr[], int n, int i, int j)
     if (dp[i][j] != -1)
         return dp[i][j];
 
-    dp[i][j] = INT_MAX;
+    dp[i][j] = 999;
     for (int k = i; k < j; k++)
         dp[i][j] = min(dp[i][j], matrixChain(arr, n, i, k) + matrixChain(arr, n, k + i, j) + arr[i - 1] * arr[k] * arr[j]);
     // c[i-j] = min(c[i-k] + c[k+1 - j] + d(i-1)*d(k)*d(j))
